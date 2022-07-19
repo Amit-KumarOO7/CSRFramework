f57d8bb64739e318a09a427c0557192e4803b7c3
Amit-KumarOO7 <amitkumar01122000@gmail.com>
2022-07-19 11:07:04 +0530

    back to start

--- csr_cert/models.py
+++ csr_cert/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ca_intermediate.models import RootCAIM
 
 # Create your models here.
 class CSR(models.Model):
     csr = models.TextField(unique=True)
-    set_issuer = models.ForeignKey('self',on_delete=models.CASCADE,null=True, blank=True)
+    set_issuer = models.ForeignKey(RootCAIM,on_delete=models.CASCADE,null=True, blank=True)
     certificate = models.TextField(unique=True)
     key = models.TextField(unique=True)
 
